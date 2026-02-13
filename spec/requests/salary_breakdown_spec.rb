e0635be15903cab30f87e5d6c84fc48594d1a64b
Krupali Patel <krupalimakwana18@gmail.com>
2026-02-13 22:35:03 +0530

    refactor: organize salary breakdown specs using country-based context blocks

--- spec/requests/salary_breakdown_spec.rb
+++ spec/requests/salary_breakdown_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Salary Breakdown API", type: :request do
   describe "GET /employees/:id/salary_breakdown" do
-    let!(:employee) do
-      Employee.create!(
-        full_name: "John Doe",
-        job_title: "Developer",
-        country: "India",
-        salary: 50000
-      )
-    end
+    context "when employee is from India" do
+      let!(:employee) do
+        Employee.create!(
+          full_name: "John Doe",
+          job_title: "Developer",
+          country: "India",
+          salary: 50000
+        )
+      end
 
-    it "calculates salary breakdown for India" do
-      get "/employees/#{employee.id}/salary_breakdown"
+      it "applies 10% deduction" do
+        get "/employees/#{employee.id}/salary_breakdown"
 
-      expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:ok)
 
-      json = JSON.parse(response.body)
+        json = JSON.parse(response.body)
 
-      expect(json["gross_salary"]).to eq(50000.0)
-      expect(json["deduction"]).to eq(5000.0)   # 10%
-      expect(json["net_salary"]).to eq(45000.0)
+        expect(json["gross_salary"]).to eq(50000.0)
+        expect(json["deduction"]).to eq(5000.0)
+        expect(json["net_salary"]).to eq(45000.0)
+      end
     end
   end
 end
