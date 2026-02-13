77039fbbb4ca90f0280459c89f57baf08839a8d2
Krupali Patel <krupalimakwana18@gmail.com>
2026-02-13 17:42:33 +0530

    feat: add salary presence validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,5 @@ class Employee < ApplicationRecord
 	validates :full_name, presence: true
 	validates :job_title, presence: true
 	validates :country, presence: true
+	validates :salary, presence: true
 end
